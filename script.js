f2d618d3a1e03aca0bfaddbc751985afe908d372
deepak516 <dk8681073@gmail.com>
2025-08-02 00:11:14 +0530

    Initial commit of simple calculator project

---
+++ script.js
@@ -0,0 +1,19 @@
+const display=document.getElementById("display")
+
+function Button(num)
+{
+  display.value += num;
+}
+
+function Clear(){
+  display.value="";
+}
+
+function Solve(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value="Enter A Valid Input";
+  }
+}
